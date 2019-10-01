1580466ee7fa26e6cff8d1b178437730ecbb4a57
shin_kim <37543606+shinaaaa@users.noreply.github.com>
2019-10-01 17:35:28 +0900

    20191001

--- mainpage/src/App.js
+++ mainpage/src/App.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import Attendance from './Attendance';
 import AttendanceResult from './AttendanceResult';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends React.Component {
 
   state = {
-    isResult: false
+    isResult: false,
+    title: 'hi'
+  }
+
+  getLoginResult = async () => {
+    const {
+      data: {
+        title
+      }
+    } = await axios.get(
+      "http://70.12.224.38:3000/index"
+    );
+    console.log("qweqweqweqwe : ",title);
+    
+    this.setState({ title });
+  };
+  componentDidMount() {
+    //this.getLoginResult();
   }
 
   AttendanceResult = () => {
     this.setState({ isResult: true})
+    this.getLoginResult();
   };
 
   render() {
-    const { isResult } = this.state;
+    const { isResult, title } = this.state;
     return (
       <div className="App">
         <center>
@@ -22,6 +42,7 @@ class App extends React.Component {
           {isResult ? <AttendanceResult /> : <Attendance />}
           <input name="Attendance_NFC" type="text" placeholder="NFC" />
           <button onClick={this.AttendanceResult}>Check</button>
+          <p>{title}</p>
         </center>
       </div>
     );
